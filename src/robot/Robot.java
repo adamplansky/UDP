69a215452fc2cac2157559d68de912609eb151a5
Adam Plánský <adamplansky@gmail.com>
2013-02-24 02:29:51 +0100

    init add

---
+++ src/robot/Robot.java
@@ -0,0 +1,15 @@
+package robot;
+
+/**
+ *
+ * @author Adam Plansky if you have some question please contact me:
+ * plansada@fit.cvut.cz
+ *
+ *
+ */
+public class Robot {
+
+    public static void main(String[] args) {
+        System.out.println("UDP ROBOT MASTER BRANCH");
+    }
+}

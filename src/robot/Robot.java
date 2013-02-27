40bd3b20169944a1f167a781682a9048f2ac2102
Adam Plánský <adamplansky@gmail.com>
2013-02-27 21:08:27 +0100

    rozpracovanyv0.0.1

--- src/robot/Robot.java
+++ src/robot/Robot.java
@@ -13,66 +13,96 @@ import java.io.*;
 //It is DEVELOPER BRANCH
 public class Robot {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
+        try {
+            Client c = new Client("192.168.10.211", 4000);
+            c.receiveScreenshot();
+            //c.sendFirmware();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
 
+class Client {
+    //identifikátor 'spojení'	sekvenční  číslo	číslo potvrzení     příznak         data
+    //  4B                                  2B                    2B              1B            0-255B
 
-        //identifikátor 'spojení'	sekvenční  číslo	číslo potvrzení     příznak         data
-        //  4B                              2B                      2B              1B              0-255B
-        final int SYN = 4;
-        final int FIN = 2;
-        final int RST = 1;
-        final int DOWNLOAD = 0x01;
-        final int UPLOAD = 0x02;
+    boolean end = false;
+    int counter = 0;
+    DatagramSocket socket;
+    DatagramPacket packet;
+    InetAddress address;
+    int port, packetLen;
+    byte[] messageSend, messageReceive;
+    Send s;
+    Receive r;
 
-
-        final ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        final DataOutputStream daos = new DataOutputStream(baos);
-        daos.writeInt(0);
-        daos.writeShort(0);
-        daos.writeShort(0);
-        daos.writeByte(SYN);
-        daos.writeByte(DOWNLOAD);
-        daos.close();
-        final byte[] bytes = baos.toByteArray();
-
-        DatagramSocket socket;
-        DatagramPacket packet;
-        InetAddress address, fromAddress;
-        String messageString;
-        byte[] message;
-
-        int fromPort, port = 4000;
-
-        //
-        // Send request
-        //
+    public Client(String address, int port) throws UnknownHostException, SocketException {
+        this.address = InetAddress.getByName(address);
+        this.port = port;
         socket = new DatagramSocket();
-        address = InetAddress.getByName("192.168.10.211");
-        message = bytes;
-        System.out.println(message.length);
-        packet = new DatagramPacket(message, message.length, address, port);
+        socket.setSoTimeout(100);
+        messageSend = new byte[264]; //264 = 4+2+2+1+255
+        messageReceive = new byte[264]; //264 = 4+2+2+1+255
+    }
+
+    public void sendFirmware() {
+    }
+
+    public void receiveScreenshot() throws IOException {
+        s = new Send("DOWN");
+        r = new Receive();
+        connectionPacket();
+        receive(); //prijme prvni packet s datama
+        r.setMessage(messageReceive,packetLen);
+        s.print();
+        r.print();
+//        while (end == false) {
+//            //prijmu packet
+//            receive();
+//            r.setMessage(messageReceive,packetLen);
+//
+//            s.print();
+//            r.print();
+//
+//            //odeslu packet
+//            messageSend = s.getMessage();
+//            send(messageSend);
+//
+//        }
+    }
+
+    public void connectionPacket() throws IOException {
+        messageSend = s.getMessage();
+        send(messageSend);
+        receive();
+        r.setMessage(messageReceive,packetLen);
+        
+        s.print();
+        r.print();
+        System.out.println("Connection established, id conn = " + Integer.toHexString(r.idCon));
+    }
+
+    private void send(byte[] msg) throws IOException {
+        packet = new DatagramPacket(msg, msg.length, this.address, this.port);
         socket.send(packet);
+    }
 
-        //
-        // Receive reply and print
-        //
-        packet = new DatagramPacket(message, message.length);
-        socket.receive(packet);
-        int length = packet.getLength();
-        fromAddress = packet.getAddress();
-        fromPort = packet.getPort();
-
-
-
-        final ByteArrayInputStream bais = new ByteArrayInputStream(packet.getData());
-        final DataInputStream dais = new DataInputStream(bais);
-
-        int IDNO = dais.readInt();
-        short seqNo = dais.readShort();
-        short confNo = dais.readShort();
-        byte falgNo = dais.readByte();
-        byte cmdNO = dais.readByte();
-        System.out.println(Integer.toHexString(IDNO).toUpperCase() + " RECV " + fromAddress.getHostAddress() + ":" + port + " seq=" + seqNo + " ack=" + confNo + " flags=" + falgNo);
-        socket.close();
+    private void receive() throws IOException {
+        packet = new DatagramPacket(messageReceive, messageReceive.length);
+        try {
+            socket.receive(packet);
+            
+        } catch (SocketTimeoutException e) {
+            if (counter++ == 20) {
+                end = true;
+            }
+            send(messageSend);
+            receive();
+        } finally {
+            counter = 0;
+            packetLen = packet.getLength();
+        }
     }
 }
